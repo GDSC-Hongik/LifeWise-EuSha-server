07ac9473482887ac6bb5d572c857dfe44006a0ed
hye-inA <hye.ina0130@gmail.com>
2025-01-07 15:07:26 +0900

    init : 프로젝트 생성

---
+++ src/main/java/team/eusha/lifewise/LifewiseApplication.java
@@ -0,0 +1,13 @@
+package team.eusha.lifewise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifewiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifewiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/eusha/lifewise/LifewiseApplicationTests.java
@@ -0,0 +1,13 @@
+package team.eusha.lifewise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifewiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
