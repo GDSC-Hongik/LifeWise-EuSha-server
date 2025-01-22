917bf6d865ffd248dfb745378f9b5a279a40a98a
hye-inA <hye.ina0130@gmail.com>
2025-01-22 20:44:36 +0900

    feat : JwtAuthenticationFilter 세팅

---
+++ src/main/java/team/eusha/lifewise/security/jwt/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package team.eusha.lifewise.security.jwt.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+    }
+
+
+}
