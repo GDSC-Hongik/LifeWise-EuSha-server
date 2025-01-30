0e29d8e142802d5b33429fc8acdd7045f38cd8eb
hye-inA <hye.ina0130@gmail.com>
2025-01-31 00:35:31 +0900

    feat : 회원가입 API 추가

--- src/main/java/team/eusha/lifewise/controller/MemberController.java
+++ src/main/java/team/eusha/lifewise/controller/MemberController.java
@@ -4,10 +4,16 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import team.eusha.lifewise.domain.Member;
 import team.eusha.lifewise.dto.request.MemberLoginRequest;
+import team.eusha.lifewise.dto.request.MemberSignupRequest;
 import team.eusha.lifewise.dto.response.MemberLoginResponse;
+import team.eusha.lifewise.dto.response.MemberSignupResponse;
 import team.eusha.lifewise.security.jwt.util.JwtTokenizer;
+import team.eusha.lifewise.service.MemberService;
 
 import java.util.List;
 
@@ -17,13 +23,36 @@ import java.util.List;
 public class MemberController {
 
     private final JwtTokenizer jwtTokenizer;
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/signup")
+    public ResponseEntity signup(@RequestBody @Valid MemberSignupRequest request, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        Member member = new Member();
+        member.setName(request.getName());
+        member.setEmail(request.getEmail());
+        member.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        Member saveMember = memberService.addMember(member);
+
+        MemberSignupResponse response = new MemberSignupResponse();
+        response.setMemberId(saveMember.getMemberId());
+        response.setName(saveMember.getName());
+        response.setCreatedAt(saveMember.getCreatedAt());
+        response.setEmail(saveMember.getEmail());
+
+        return new ResponseEntity(response, HttpStatus.CREATED);
+    }
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid MemberLoginRequest login) {
 
         Long memberId = 1L;
         String email = login.getEmail();
-        List<String> roles = List.of("ROLE_USER");
+        List<String> roles = List.of("USER");
 
         // JWT 토큰 생성
         String accessToken = jwtTokenizer.createAccessToken(memberId, email, roles);
