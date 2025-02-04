c78aede6b9bb47d063c4328a890093dee2ed6a54
hye-inA <hye.ina0130@gmail.com>
2025-02-04 12:14:36 +0900

    refactor : 회원가입 응답 날짜형식 수정

--- src/main/java/team/eusha/lifewise/dto/response/MemberSignupResponse.java
+++ src/main/java/team/eusha/lifewise/dto/response/MemberSignupResponse.java
@@ -1,13 +1,15 @@
 package team.eusha.lifewise.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class MemberSignupResponse {
-    private String memberName;
     private Long memberId;
+    private String memberName;
     private String email;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 }
