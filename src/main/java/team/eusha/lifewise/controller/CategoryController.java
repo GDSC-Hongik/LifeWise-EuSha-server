39216001271ee01c378aaae2641a3c03a9905bfb
hye-inA <hye.ina0130@gmail.com>
2025-02-12 23:57:31 +0900

    feat(api) : 카테고리 조회 API 생성

---
+++ src/main/java/team/eusha/lifewise/controller/CategoryController.java
@@ -0,0 +1,48 @@
+package team.eusha.lifewise.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.eusha.lifewise.dto.response.DetailResponse;
+import team.eusha.lifewise.dto.response.MainCategoryListResponse;
+import team.eusha.lifewise.dto.response.SubCategoryListResponse;
+import team.eusha.lifewise.service.CategoryService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<MainCategoryListResponse>> getAllCategories() {
+        return ResponseEntity.ok(categoryService.getAllCategories());
+    }
+
+    @GetMapping("/{categoryId}/subcategories")
+    public ResponseEntity<SubCategoryListResponse> getSubCategories(
+            @PathVariable Long categoryId) {
+        return ResponseEntity.ok(categoryService.getSubCategories(categoryId));
+    }
+
+    @GetMapping("/{categoryId}/subcategories/all")
+    public ResponseEntity<List<? extends DetailResponse>> getAllCategoryDetails(
+            @PathVariable Long categoryId
+    ) {
+        return ResponseEntity.ok(categoryService.getAllDetails(categoryId));
+    }
+
+    @GetMapping("/{categoryId}/subcategories/{subCategoryId}")
+    public ResponseEntity<List<? extends DetailResponse>> getSubCategoryDetails(
+            @PathVariable Long categoryId,
+            @PathVariable Long subCategoryId
+    ) {
+        return ResponseEntity.ok(categoryService.getSubCategoryDetails(categoryId, subCategoryId));
+    }
+}
