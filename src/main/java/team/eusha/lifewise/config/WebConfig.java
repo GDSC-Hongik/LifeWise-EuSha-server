586e2af5daa23e59609376be09ea61ba48164b61
hye-inA <hye.ina0130@gmail.com>
2025-02-17 14:26:20 +0900

    feat : Vercel 도메인 추가

--- src/main/java/team/eusha/lifewise/config/WebConfig.java
+++ src/main/java/team/eusha/lifewise/config/WebConfig.java
@@ -3,15 +3,16 @@ package team.eusha.lifewise.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-// Spring MVC 에 대한 설정파일. 웹에 대한 설정파일
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    // CORS
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://life-wise-eu-sha-web.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
