6ef2b1bae0d4596f3745d35d79996a5c176abf7b
Yun Seohyun <seohyunyun325@gmail.com>
2025-02-20 18:22:21 +0900

    feat: Like 목록 응답 값 구현

---
+++ src/main/java/team/eusha/lifewise/dto/response/LikeListResponse.java
@@ -0,0 +1,59 @@
+package team.eusha.lifewise.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import team.eusha.lifewise.domain.Like;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+public class LikeListResponse {
+    private List<LikeResponse> likes;
+
+    @Getter
+    @Builder
+    public static class LikeResponse {
+        private Long likeId;
+        private DetailInfo detail;
+        private LocalDateTime createdAt;
+
+        @Getter
+        @Builder
+        public static class DetailInfo {
+            private Long id;
+            private String imageUrl;
+            private String title;
+            private List<String> description;
+        }
+
+        public static LikeListResponse.LikeResponse from(Like like) {
+            String[] descriptions = like.getDetail().getDescription().split("\n");
+            List<String> descriptionList = List.of(descriptions);
+
+            return LikeListResponse.LikeResponse.builder()
+                    .likeId(like.getId())
+                    .detail(LikeListResponse.LikeResponse.DetailInfo.builder()
+                            .id(like.getDetail().getId())
+                            .imageUrl(like.getDetail().getImageUrl())
+                            .title(like.getDetail().getTitle())
+                            .description(descriptionList)
+                            .build())
+                    .createdAt(like.getCreatedAt())
+                    .build();
+        }
+    }
+
+    public static LikeListResponse from(List<Like> likes) {
+        List<LikeListResponse.LikeResponse> responses = likes.stream()
+                .map(LikeListResponse.LikeResponse::from)
+                .collect(Collectors.toList());
+
+        return LikeListResponse.builder()
+                .likes(responses)
+                .build();
+    }
+
+}
